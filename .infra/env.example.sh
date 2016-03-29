9071377570a97d599f7fc769f4d26b45d03020cb
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-03-29 13:36:57 +0200

    Remove useless env code

--- .infra/env.example.sh
+++ .infra/env.example.sh
@@ -1,12 +1,6 @@
 # Rename this file to env.sh, it will be kept out of Git.
 # So suitable for adding secret keys and such
 
-# Set magic variables for current FILE & DIR
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
-__rootdir="${__dir}"
-
 # export DEBUG="frey:*"
 # export FREY_DOMAIN="master.tus.io"
 # export FREY_ENCRYPTION_SECRET="***"
